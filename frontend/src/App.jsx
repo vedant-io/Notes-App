03c0a8fe04810e8c1f008b556f3d23761ce852fd
vedant-io <vedantd004@gmail.com>
2024-07-10 19:19:30 +0530

    Started with making a Vite-React application and made some starting pages for the Notes application initially and then added frontend functionality to Login page and Now going to work on the SignUp Page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import Home from "./pages/Home/Home"
+import Login from "./pages/Login/Login"
+import Signup from "./pages/SignUp/Signup"
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+const router = (
+  <Router>
+    <Routes>
+      <Route path='/dashboard' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<Signup />} />
+    </Routes>
+  </Router>
+)
+
+
+function App() {
+
+  return (
+    <>
+      {router}
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='text-2xl font-bold underline bg-blue-900 text-yellow-500'>Home</div>
+  )
+}
+
+export default Home
